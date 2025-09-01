f727898dcb908636eedb1bf8b7698b401f98b88b
Tokusei Noborio <tokusei@nyampass.com>
2025-09-01 17:45:53 +0900

    fix main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_ascii_terminal::{Terminal, TerminalCamera};
 
 mod bundle;
 mod config;
@@ -26,8 +27,12 @@ pub const UI_SIZE: [u32; 2] = [VIEWPORT_SIZE[0], 8];
 pub const GAME_SIZE: [u32; 2] = [VIEWPORT_SIZE[0], VIEWPORT_SIZE[1] - UI_SIZE[1]];
 
 fn setup(mut commands: Commands) {
+    commands.spawn(Terminal::new(VIEWPORT_SIZE));
+    commands.spawn(TerminalCamera::new());
+
     //commands.spawn().insert(gen.map);
 
+    /*
     let term_y = VIEWPORT_SIZE[1] as f32 / 2.0 - GAME_SIZE[1] as f32 / 2.0;
     let term_bundle = TerminalBundle {
         transform: Transform::from_xyz(0.0, term_y, 0.0),
@@ -39,23 +44,23 @@ fn setup(mut commands: Commands) {
     let totalx = GAME_SIZE[0];
     let totaly = GAME_SIZE[1] + UI_SIZE[1];
     commands.spawn_bundle(TiledCameraBundle::new().with_tile_count([totalx, totaly]));
+     */
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(player::PlayerPlugin)
-        .add_plugin(map::MapGenPlugin)
-        .add_plugin(render::RenderPlugin)
-        .add_plugin(events::EventsPlugin)
-        .add_plugin(visibility::VisiblityPlugin)
-        .add_plugin(map_state::MapStatePlugin)
-        //.add_plugin(web_resize::FullViewportPlugin)
-        .add_plugin(turn_system::TurnSystemPlugin)
-        .add_plugin(monster::MonstersPlugin)
-        .add_plugin(combat::CombatPlugin)
-        .add_plugin(ui::UiPlugin)
-        .add_startup_system(setup)
+        .add_plugins(player::PlayerPlugin)
+        .add_plugins(map::MapGenPlugin)
+        .add_plugins(render::RenderPlugin)
+        .add_plugins(events::EventsPlugin)
+        .add_plugins(visibility::VisiblityPlugin)
+        .add_plugins(map_state::MapStatePlugin)
+        .add_plugins(turn_system::TurnSystemPlugin)
+        .add_plugins(monster::MonstersPlugin)
+        .add_plugins(combat::CombatPlugin)
+        .add_plugins(ui::UiPlugin)
+        .add_systems(Startup, setup)
         .insert_resource(ClearColor(Color::BLACK))
         .run();
 }
